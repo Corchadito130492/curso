4d12f321236323471f4f3bae6cbeda525037e75c
ICIDESI\MATAYU1 <corchaditomata130492@gmail.com>
2018-06-06 13:20:13 -0500

    ejercicios 2 y 3

---
+++ HTML-CSS/practica03/js/index.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  $('#mensajeConfirmacion').hide();
+
+  $('#mensajeCancelacion').fadeOut();
+
+  $('#btn_Guardar').click(function(){
+    var nombre = $('#txt_Nombre').val();
+    var apellidos = $('#txt_Apellidos').val();
+    var sexo = $('#Lista_sexo').val();
+
+    $('#lbl_nombre').text(nombre);
+    $('#lbl_apellidos').text(apellidos);
+    $('#lbl_sexo').text(sexo);
+
+    $('#mensajeConfirmacion').show();
+    limpiaCampos();
+  });
+
+  $('#btn_Cancelar').click(function(){
+  //mostramos el mensaje de cancelacion
+  $('#mensajeCancelacion').fadeIn();
+});
+
+function limpiaCampos(){
+  $('#txt_Nombre').val("");
+$('#txt_Apellidos').val("");
+$('#txt_Correo').val("");
+$('#Lista_sexo').val("");
+$('#txt_Ocupacion').val("");
+}
+
+
+});
